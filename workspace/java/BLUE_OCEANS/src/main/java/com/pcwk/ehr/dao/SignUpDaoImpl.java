2f7b2b45ba0ff46f0d3ff14390db41f37379e5c3
조재환 <ccjj1002@naver.com>
2023-08-17 12:38:44 +0900

    회원가입시 otp, 동의테이블 데이터 자동입력

--- workspace/java/BLUE_OCEANS/src/main/java/com/pcwk/ehr/dao/SignUpDaoImpl.java
+++ workspace/java/BLUE_OCEANS/src/main/java/com/pcwk/ehr/dao/SignUpDaoImpl.java
@@ -26,34 +26,42 @@ public class SignUpDaoImpl implements SignUpDao, PcwkLogger {
 	public int add(UserVO userVO) throws SQLException, ClassNotFoundException {
 		
 		int flag = 0;
-		int flag1 = 0;
-		int flag2 = 0;
+		int add_flag = 0;
+		int otp_flag = 0;
+		int agree_flag = 0;
 		
 		
 		 
 		String member = this.NAMESPACE+DOT+"add";
-		
-		
 		LOG.debug("┌─────────────────┐");
-		LOG.debug("│DaoImpl구역                 │"+member);
+		LOG.debug("│DaoImpl구역       │"+member);
 		LOG.debug("└─────────────────┘");
 		LOG.debug("SignUpVO 값 ==" + userVO.toString());
-		
-		flag1 = this.sqlSessionTemplate.insert(member, userVO); 
+
+		add_flag = this.sqlSessionTemplate.insert(member, userVO);
 		
 		
 		String otp = this.NAMESPACE+DOT+"otp";
 		LOG.debug("┌─────────────────┐");
-		LOG.debug("│DaoImpl구역                 │"+otp);
+		LOG.debug("│DaoImpl구역       │"+otp);
 		LOG.debug("└─────────────────┘");
-		LOG.debug("SignUpVO 값 ==" + userVO.toString());
+		LOG.debug("otp 값 ==" + userVO.toString());
 		
-		flag2 = this.sqlSessionTemplate.insert(otp, userVO); 
+		otp_flag = this.sqlSessionTemplate.insert(otp, userVO);
+
+		String agree = this.NAMESPACE+DOT+"agree";
+		LOG.debug("┌─────────────────┐");
+		LOG.debug("│DaoImpl구역       │"+agree);
+		LOG.debug("└─────────────────┘");
+		LOG.debug("agree 값 ==" + userVO.toString());
+
+		agree_flag = this.sqlSessionTemplate.insert(agree, userVO);
+
+		LOG.debug("DaoImple flag 값 = " + add_flag);
+		LOG.debug("DaoImple flag 값 = " + otp_flag);
+		LOG.debug("DaoImple flag 값 = " + agree_flag);
 		
-		LOG.debug("DaoImple flag 값 = " + flag1);
-		LOG.debug("DaoImple flag 값 = " + flag2);
-		
-		if (flag1 == 1 && flag2 == 1) {
+		if (add_flag == 1 && otp_flag == 1 && agree_flag == 1) {
 			flag = 1;
 		}
 		else {

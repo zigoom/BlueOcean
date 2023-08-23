e4730c3569e68e669e5c3a5f5592c4c6fea0889a
최경원 <qpwoei0112@naver.com>
2023-08-23 16:39:36 +0900

    마이페이지 로그 코드 작성

--- workspace/java/BLUE_OCEANS/src/main/java/com/pcwk/ehr/controller/MyPageController.java
+++ workspace/java/BLUE_OCEANS/src/main/java/com/pcwk/ehr/controller/MyPageController.java
@@ -1,5 +1,6 @@
 package com.pcwk.ehr.controller;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,8 +17,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pcwk.ehr.cmn.PcwkLogger;
+import com.pcwk.ehr.service.AdminLogService;
 import com.pcwk.ehr.service.MyPageService;
-import com.pcwk.ehr.cmn.BookmarkVO;
+import com.pcwk.ehr.cmn.AdminPageVO;
 import com.pcwk.ehr.cmn.MyPageVO;
 
 @Controller("MyPageController")
@@ -26,32 +28,46 @@ public class MyPageController implements PcwkLogger {
 
 	@Autowired
 	private MyPageService myPageService;
+	
+	@Autowired
+	AdminLogService adminLogService;
 
 	@RequestMapping(value = "/mypage.do", method = RequestMethod.GET)
-	public String mypageView(Model model, HttpSession session) {
-
+	public String mypageView(Model model, HttpSession session) throws SQLException {
 		LOG.debug("┌─────────────────┐");
 		LOG.debug("│ mypageView      │");
 		LOG.debug("│ getAttribute : " + session.getAttribute("user"));
 		LOG.debug("└─────────────────┘");
-
+		
 		String user = (String) session.getAttribute("user");
 
-	
 		LOG.debug("getAttribute : " + user);
 	   	
 
-		MyPageVO userInfo = myPageService.getMemberById(user); // 테스트용 사용자 아이디
+		MyPageVO userInfo = myPageService.getMemberById(user); 
 
 		LOG.debug("userInfo - " + userInfo);
 		model.addAttribute("userinfo", userInfo);
+		
+		if (null != session.getAttribute("user")) {
+			AdminPageVO logVO = new AdminPageVO();
+			String id = (String) session.getAttribute("user");
+			logVO.setUserId(id);
+			logVO.setLog1("마이페이지 이동");
+			
+			logVO.setLog2("이동");
+			
+			LOG.debug("LOG확인 : "+ logVO);
+			
+			adminLogService.addLog(logVO);
+		}
 
 		return "main/mypage";
 	}
 
 	@RequestMapping(value = "/userUpdate.do", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody // JSON 형태의 응답을 반환하기 위해 필요
-	public Map<String, String> userUpdate(@RequestBody MyPageVO myPageVO, HttpSession session) {
+	public Map<String, String> userUpdate(@RequestBody MyPageVO myPageVO, HttpSession session) throws SQLException {
 		LOG.debug("┌────────────────┐");
 		LOG.debug("│ userUpdate     │");
 		LOG.debug("└────────────────┘");
@@ -73,6 +89,22 @@ public class MyPageController implements PcwkLogger {
 		} catch (Exception e) {
 			resultMap.put("result", "failure"); // 실패 시 "failure" 반환
 		}
+		
+		if (null != session.getAttribute("user")) {
+			AdminPageVO logVO = new AdminPageVO();
+			String id = (String) session.getAttribute("user");
+			logVO.setUserId(id);
+			logVO.setLog1("회원정보 수정");
+			
+			logVO.setLog2("변경 정보 [phoneNo :"+myPageVO.getPhoneNo()+
+						  ",birthday :"+myPageVO.getBirthday()+
+						  ",gender :"+myPageVO.getGender()+
+						  ",email :"+ myPageVO.getEmail()+
+						  ",keyword : "+ myPageVO.getKeyword()+"]");
+				
+			
+			adminLogService.addLog(logVO);
+		}
 
 		return resultMap; // JSON 형태의 응답 데이터 반환
 	}
@@ -82,7 +114,7 @@ public class MyPageController implements PcwkLogger {
 			 		,produces = "application/json;charset=UTF-8")
 	 
 	  @ResponseBody public Map<String, String> withdraw(@RequestBody MyPageVO
-	  myPageVO , HttpSession session){ 
+	  myPageVO , HttpSession session) throws SQLException{ 
 		LOG.debug("┌────────────────┐");
 		LOG.debug("│ withdraw       │");
 		LOG.debug("└────────────────┘");
@@ -94,6 +126,17 @@ public class MyPageController implements PcwkLogger {
 		   String userId = (String) session.getAttribute("user");
 		   myPageVO.setUserId(userId);
 	  
+			  if (null != session.getAttribute("user")) {
+					AdminPageVO logVO = new AdminPageVO();
+					String id = (String) session.getAttribute("user");
+					logVO.setUserId(id);
+					logVO.setLog1("회원탈퇴");
+					
+					logVO.setLog2("탈퇴한 회원: "+ myPageVO.getUserId());
+						
+					
+					adminLogService.addLog(logVO);
+				}
 		   myPageService.withdraw(myPageVO);
 		   resultMap.put("result", "success"); // 성공 시"success" 반환
 		   
@@ -107,7 +150,9 @@ public class MyPageController implements PcwkLogger {
 		   resultMap.put("result", "failure"); //실패 시 "failure" 반환
 	   }
 	  
-	  return resultMap; }
-	 
 
+	  
+	  return resultMap;
+	  }
+	 
 }
